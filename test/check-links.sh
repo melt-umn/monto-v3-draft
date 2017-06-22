2f304b2a71694b930e3b24d190f0cc26407e0ca3
Nathaniel Ringo <remexre@gmail.com>
2017-06-22 12:02:07 -0500

    Adds draft02.

--- test/check-links.sh
+++ test/check-links.sh
@@ -2,8 +2,7 @@
 
 set -eu
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
-cd ..
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 command -v htmltest >/dev/null || go get -u github.com/wjdp/htmltest
 command -v hugo >/dev/null || go get -u github.com/gohugoio/hugo
